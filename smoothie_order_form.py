0e030997a8a0e916036deab36ed4e1314f88b2c8
nitinmeruva2005 <147080458+nitinmeruva2005@users.noreply.github.com>
2026-08-14 10:06:22 +0530

    Comment out smoothie response display
    Comment out the line displaying smoothie data.

--- smoothie_order_form.py
+++ smoothie_order_form.py
@@ -35,6 +35,6 @@ if ingredients_list :
           st.success('Your Smoothie is ordered!,'''+title+'!',icon="✅") 
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-st.text(smoothiefroot_response.json())
+#st.text(smoothiefroot_response.json())
 sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
 
