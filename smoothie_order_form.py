70ddde8d6a611de0e6da8ac70bb3f7563c8d2bd4
nitinmeruva2005 <147080458+nitinmeruva2005@users.noreply.github.com>
2026-08-14 09:41:36 +0530

    Create smoothie order form with Streamlit

---
+++ smoothie_order_form.py
@@ -0,0 +1,36 @@
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
+# Write directly to the app
+st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
+st.write(
+  """Choose the fruits you want in your custom Smoothie!
+  """
+)
+
+
+title = st.text_input("Name on Smoothie")
+#st.write("The name on yout Smoothie will be:",title)
+
+session = get_active_session()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+#st.dataframe(data=my_dataframe, use_container_width=True)
+
+ingredients_list=st.multiselect('Choose up to 5 ingredients:',my_dataframe,max_selections=5)
+
+
+if ingredients_list :                           
+        ingredients_string=''
+        for fruit_chosen in ingredients_list:
+            ingredients_string+=fruit_chosen + ' '
+        #st.write(ingredients_string)
+        order_filled=False
+        my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+                    values ('""" + ingredients_string + """','"""+title+"""')"""
+
+        #st.write(my_insert_stmt)
+        submit_button=st.button("Submit Order")
+        if submit_button:
+          session.sql(my_insert_stmt).collect()
+          st.success('Your Smoothie is ordered!,'''+title+'!',icon="✅") 
